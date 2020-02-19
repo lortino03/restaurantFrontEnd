41b43b04e4c58df3281b1dd59961a91c73ab57e8
lortino03 <lortino.ansalla@yahoo.fr>
2020-02-19 10:27:24 +0100

    methodes ajout client tester et fonctionnelle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { PlatsComponent } from './plats/plats.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { ManagementComponent } from './management/management.component';
-import { NgModule } from '@angular/core';
 import{FormsModule} from '@angular/forms'
 
 @NgModule({
